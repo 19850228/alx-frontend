ac6070704f68a271516c9c015a75e4cc95033d03
19850228 <fmthombothi@yahoo.com>
2024-07-09 16:27:46 +0000

    front-end

---
+++ 3-article.html/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env', // Use the '@babel/preset-env' preset to automatically determine the Babel plugins needed based on the environment
+      {
+        targets: {
+          node: 'current', // Target the current version of Node.js
+        },
+      },
+    ],
+  ],
+};
